54f716570f2030cee35c81360ad4d5c1e14c6911
Arvind Chauhan <40683239+arvind520@users.noreply.github.com>
2022-04-24 13:05:08 +0530

    edit functionality is added

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,15 +1,18 @@
 import Card from "./shared/Card";
-import { FaTimes } from "react-icons/fa";
+import { FaTimes, FaEdit } from "react-icons/fa";
 import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
 
 const FeedbackItem = ({ item }) => {
-  const {deleteHandle} = useContext(FeedbackContext)
+  const { deleteHandle, editFeedback } = useContext(FeedbackContext);
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
       <button onClick={() => deleteHandle(item.id)} className="close">
         <FaTimes color="purple" />
+      </button>
+      <button onClick={() => editFeedback(item)} className="edit">
+        <FaEdit color="purple" />
       </button>
       <div className="text-display">{item.text}</div>
     </Card>
